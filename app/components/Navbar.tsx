fa5acb2c1478b1fadfa1edb9f42ced96c000b268
Nicholas Yang <nick@nicholasyang.com>
2022-10-15 14:56:06 -0400

    Some fixes and deletions

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import ChapterDropdown from "~/components/ChapterDropdown";
 import { useState } from "react";
-import {Link} from "@remix-run/react";
+import { Link } from "@remix-run/react";
 
 export default function Navbar() {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   return (
     <div className="w-full bg-black flex justify-evenly pb-2 pt-2">
       <div className="font-william text-white text-2xl">
-        <Link to="/">Data by Design</a>
+        <Link to="/">Data by Design</Link>
       </div>
       <div className="flex justify-around w-3/5 font-sans text-white text-xl">
         <div
