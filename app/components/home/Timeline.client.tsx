afed550e505a92f50d3a1217c8b6108f729eeb3e
Nicholas Yang <nick@nicholasyang.com>
2023-01-10 15:46:01 -0500

    Added timeline (#18)

---
+++ app/components/home/Timeline.client.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import DraggableTimeline from "~/components/home/DraggableTimeline";
+import OrderedTimeline from "~/components/home/OrderedTimeline";
+import type { Image } from "./timelineUtils";
+import imageData from "~/data/timelineImages.json";
+import { Link } from "@remix-run/react";
+
+enum TimelineType {
+  Draggable,
+  Ordered,
+}
+
+export default function Timeline() {
+  const [timelineType, setTimelineType] = useState(TimelineType.Ordered);
+  const [selectedImage, setSelectedImage] = useState<Image>(
+    () => imageData[Math.floor(Math.random() * imageData.length)]
+  );
+
+  return (
+    <div className="text-white text-center font-william text-3xl py-4">
+      <h1>TIMELINE</h1>
+      <div className="flex justify-center">
+        <button onClick={() => setTimelineType(TimelineType.Draggable)}>
+          <img
+            className="w-10"
+            src={
+              timelineType === TimelineType.Draggable
+                ? "/images/ui/shuffle_click.png"
+                : "/images/ui/shuffle_unclick.png"
+            }
+          />
+        </button>
+        <button onClick={() => setTimelineType(TimelineType.Ordered)}>
+          <img
+            className="w-10"
+            src={
+              timelineType === TimelineType.Ordered
+                ? "/images/ui/sort_selected.png"
+                : "/images/ui/sort_unselected.png"
+            }
+          />
+        </button>
+      </div>
+      {timelineType === TimelineType.Draggable ? (
+        <DraggableTimeline setSelectedImage={setSelectedImage} />
+      ) : (
+        <OrderedTimeline setSelectedImage={setSelectedImage} />
+      )}
+      <div className="bg-brooksSecondary flex p-10">
+        <img
+          className="w-2/5"
+          src={`/images/${selectedImage.CHAPTER}/${selectedImage.FILE_NAME}`}
+        />
+        <div className="p-20 font-william w-3/5 text-black text-left">
+          <div className="text-4xl p-5">
+            {selectedImage.TITLE} by {selectedImage.ARTIST} (
+            {selectedImage.YEAR})
+          </div>
+          <div className="text-lg p-5">
+            <span>{selectedImage.CREDIT} </span>
+            <span>{selectedImage.DIGITIZED}</span>
+          </div>
+          <Link
+            to={`/chapters/${selectedImage.CHAPTER}`}
+            className="text-lg p-5"
+          >
+            Go to chapter -&gt;
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/home/timelineUtils.ts
@@ -0,0 +1,10 @@
+export interface Image {
+  CHAPTER: string;
+  FILE_NAME: string;
+  TITLE: string;
+  ARTIST: string;
+  CREDIT: string;
+  DIGITIZED: string;
+  YEAR: string;
+  "ALT-TEXT": string;
+}
