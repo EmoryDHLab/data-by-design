079d5ae96c12165301987782bb93933f4d2a2a29
yuchi <chronictanvi@gmail.com>
2023-03-14 18:53:46 -0400

    Revert "Permutation 2"
    This reverts commit 758ba56053004d54511e9a10e9f44b7feef0baae.

--- app/components/home/HomeTitle.tsx
+++ app/components/home/HomeTitle.tsx
@@ -1,46 +1,50 @@
-// import ImageModal from "~/components/layout/ImageModal";
-// export default function HomeTitle() {
-//   return (
-//     <div className="sm:grid bg-black text-white sm:grid-cols-14 sm:grid-rows-30 font-dubois pt-10 sm:pt-5">
-//       <div className="flex flex-col items-center col-start-11 col-span-4 bg-nightingale_blue sm:p-16 pb-5 pt-10">
-//         <div className="flex flex-col items-center">
-//           <ImageModal
-//             src="/images/ch5-08b-amalg.jpg"
-//             className="w-3/5 sm:w-full"
-//           />
-//         </div>
-//         <div className="caption text-center sm:pt-10 py-5 w-5/6 sm:text-lg sm:w-full text-sm">
-//           <span className="text-offwhite">
-//             The Amalgamation of White and Black elements of the population in
-//             the United States by W.E.B Du Bois. Atlanta University. Library of
-//             Congress.
-//           </span>
-//         </div>
-//       </div>
-//       <div className="sm:row-start-1 sm:col-start-1 sm:col-end-11 sm:grid sm:grid-cols-5 sm:grid-rows-5 pl-6 sm:pl-0 sm:row-span-8">
-//         <div className="py-10 font-bold text-5xl sm:text-frontTitle sm:col-start-1 sm:col-span-4 sm:row-span-3 z-10 leading-veryTight sm:pl-24">
-//           <span className="sm:hidden">DATA BY DESIGN</span>
-//           <span className="hidden sm:inline">DATA BY</span>{" "}
-//           <span className="hidden sm:ml-32 sm:inline">DESIGN</span>
-//         </div>
-//         <div className="sm:text-3xl text-xl font-semibold sm:font-normal col-start-4 col-span-2 row-start-4">
-//           An Interactive History
-//           <br /> of Data Visualization
-//           <br />
-//           1786-1900
-//         </div>
-//         <div className="flex py-10 font-dubois font-light pb-15 sm:p-0 sm:contents justify-between col-start-4 pr-8">
-//           <div className="sm:text-xl italic text-sm sm:col-start-2 sm:col-span-2 sm:row-start-5">
-//             What is the story we tell
-//             <br /> about the rise of modern
-//             <br /> data visualization?
-//           </div>
-//           <div className="sm:text-xl  italic text-sm sm:col-start-4 sm:col-span-2 sm:row-start-5">
-//             How might we tell that
-//             <br /> story differently?
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+import ImageModal from "~/components/layout/ImageModal";
+
+export default function HomeTitle() {
+  return (
+    <div className="sm:grid bg-black text-white sm:grid-cols-14 sm:grid-rows-30 font-dubois pt-10 sm:pt-32">
+      {/* <div className="flex flex-col items-center col-start-11 col-span-4 bg-nightingale_blue sm:p-16 pb-5 pt-10">
+        <div className="flex flex-col items-center">
+          <ImageModal
+            src="/images/ch5-08b-amalg.jpg"
+            className="w-3/5 sm:w-full"
+          />
+        </div>
+        <div className="caption text-center sm:pt-10 py-5 w-5/6 sm:text-lg sm:w-full text-sm">
+          <span className="text-offwhite">
+            The Amalgamation of White and Black elements of the population in
+            the United States by W.E.B Du Bois. Atlanta University. Library of
+            Congress.
+          </span>
+        </div>
+      </div> */}
+      <div className="sm:row-start-1 sm:col-start-1 sm:col-end-11 sm:grid sm:grid-cols-5 sm:grid-rows-4 pl-6 sm:pl-0 sm:row-span-8">
+        <div className="py-10 font-bold text-5xl sm:text-frontTitle sm:col-start-1 sm:col-span-7 sm:row-span-3 z-10 leading-veryTight sm:pl-24">
+          <span className="sm:hidden">DATA BY DESIGN</span>
+          <span className="hidden sm:inline ">DATA BY</span>{" "}
+          <span className="hidden sm:ml-46 sm:inline">DESIGN</span>
+        </div>
+        <div className="sm:text-3xl text-xl font-semibold sm:font-normal col-start-13 col-span-2 row-start-2 row-span-3">
+          An Interactive History
+          <br /> of Data Visualization
+          <br />
+          <span className="font-light text-lg sm:text-2xl">
+            {" "}
+            1786 &ndash; 1900
+          </span>
+        </div>
+        {/* <div className="flex py-10 font-dubois font-light pb-15 sm:p-0 sm:contents justify-between col-start-4 pr-8">
+          <div className="sm:text-xl italic text-sm sm:col-start-2 sm:col-span-2 sm:row-start-5">
+            What is the story we tell
+            <br /> about the rise of modern 
+            <br /> data visualization?
+          </div>
+          <div className="sm:text-xl  italic text-sm sm:col-start-4 sm:col-span-2 sm:row-start-5">
+            How might we tell that
+            <br /> story differently?
+          </div> 
+       </div> */}
+      </div>
+    </div>
+  );
+}
--- app/components/home/Timeline.client.tsx
+++ app/components/home/Timeline.client.tsx
@@ -19,102 +19,86 @@ export default function Timeline() {
   const [shouldShuffle, setShouldShuffle] = useState(false);
 
   return (
-    <>
-      <div className="sm:grid bg-black text-white sm:grid-cols-14 sm:grid-rows-30  pt-10 sm:pt-5">
-        <div className="flex flex-col items-center col-start-11 col-span-4 bg-nightingale_blue sm:p-10 pb-5 pt-20">
+    <div>
+      <div className="sm:grid bg-black text-white text-center sm:grid-cols-14 sm:grid-rows-30 font-dubois pt-10 sm:pt-32">
+        <div className="  col-start-1 col-span-10 justify-center">
+          <h1 className="text-3xl text-center">TIMELINE</h1>
+          <button
+            onClick={() => {
+              setTimelineType(TimelineType.Draggable);
+              setShouldShuffle((shouldShuffle) => !shouldShuffle);
+            }}
+            type="button"
+          >
+            <img
+              className="w-14 m-2"
+              src={
+                timelineType === TimelineType.Draggable
+                  ? "/images/ui/shuffle_click.png"
+                  : "/images/ui/shuffle_unclick.png"
+              }
+              alt="Shuffle"
+            />
+          </button>
+          <button
+            type="button"
+            onClick={() => setTimelineType(TimelineType.Ordered)}
+          >
+            <img
+              className="w-14 m-2"
+              src={
+                timelineType === TimelineType.Ordered
+                  ? "/images/ui/sort_selected.png"
+                  : "/images/ui/sort_unselected.png"
+              }
+              alt="Sort"
+            />
+          </button>
+
+          {timelineType === TimelineType.Draggable ? (
+            <DraggableTimeline
+              selectedImage={selectedImage}
+              setSelectedImage={setSelectedImage}
+              shouldShuffle={shouldShuffle}
+            />
+          ) : (
+            <OrderedTimeline
+              selectedImage={selectedImage}
+              setSelectedImage={setSelectedImage}
+            />
+          )}
+        </div>
+
+        <div className="flex flex-col items-center col-start-11 col-span-4 bg-nightingale_blue sm:p-16 pb-5 pt-10">
           <div className="flex flex-col items-center">
             <Figure
-              className="w-3/5 sm:w-full pt-10"
+              className="w-2/5 md:w-4/5"
               src={`/images/${selectedImage.CHAPTER}/${selectedImage.FILE_NAME}`}
               alt={selectedImage.ALT_TEXT}
             />
-            <div className=" pb-5 w-full text-white text-left">
-              <div className=" font-dubois text-xl">
+            <div className="caption text-center sm:pt-5 py-3 w-5/6 sm:text-lg sm:w-full text-sm">
+              <div className="text-lg">
                 {selectedImage.TITLE}
                 <br></br>
-                <div className="font-dubois italic text-base pt-2">
+                <div className="font-dubois italic text-base pt-4">
                   by {selectedImage.ARTIST} ({selectedImage.YEAR})
                 </div>
               </div>
-              <div className="  py-2 w-5/6 sm:text-base sm:w-full text-sm">
+              <div className="font-sans text-sm p-4">
                 <span>{selectedImage.CREDIT} </span>
                 <br></br>
                 <span>{selectedImage.DIGITIZED}</span>
               </div>
               <Link
                 to={`/chapters/${selectedImage.CHAPTER}`}
-                className="text-base font-dubois  pt-6"
+                className="text-lg p-5"
               >
                 GO TO CHAPTER →
               </Link>
             </div>
           </div>
         </div>
-
-        <div className="sm:row-start-1 sm:col-start-1 sm:col-end-11 sm:grid sm:grid-cols-5 sm:4 pl-6 sm:pl-0 sm:row-span-8">
-          <div className="pt-20 font-bold text-5xl sm:text-frontTitle sm:col-start-1 font-dubois  sm:col-span-4 sm:row-span-1 z-10 leading-veryTight sm:pl-24">
-            <span className="sm:hidden">DATA BY DESIGN</span>
-            <span className="hidden sm:inline sm:pt-10">DATA BY</span>{" "}
-            <span className="hidden sm:ml-32 sm:inline">DESIGN</span>
-          </div>
-          <div className="sm:text-3xl text-xl font-dubois font-normal sm:font-normal  col-start-3 col-span-2 pt-3 row-start-2">
-            An Interactive History
-            <br /> of Data Visualization
-            <br />
-            <span className="font-light pt-5">1786-1900</span>
-            <div className="pt-10">
-              {/* <h1>TIMELINE</h1> */}
-              <button
-                onClick={() => {
-                  setTimelineType(TimelineType.Draggable);
-                  setShouldShuffle((shouldShuffle) => !shouldShuffle);
-                }}
-                type="button"
-              >
-                <img
-                  className="w-14 m-2"
-                  src={
-                    timelineType === TimelineType.Draggable
-                      ? "/images/ui/shuffle_click.png"
-                      : "/images/ui/shuffle_unclick.png"
-                  }
-                  alt="Shuffle"
-                />
-              </button>
-              <button
-                type="button"
-                onClick={() => setTimelineType(TimelineType.Ordered)}
-              >
-                <img
-                  className="w-14 m-2"
-                  src={
-                    timelineType === TimelineType.Ordered
-                      ? "/images/ui/sort_selected.png"
-                      : "/images/ui/sort_unselected.png"
-                  }
-                  alt="Sort"
-                />
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
-
-      <div className="text-white text-center font-duboisWide text-3xl p-10 bg-black">
-        <div className="flex justify-center p-3"></div>
-        {timelineType === TimelineType.Draggable ? (
-          <DraggableTimeline
-            selectedImage={selectedImage}
-            setSelectedImage={setSelectedImage}
-            shouldShuffle={shouldShuffle}
-          />
-        ) : (
-          <OrderedTimeline
-            selectedImage={selectedImage}
-            setSelectedImage={setSelectedImage}
-          />
-        )}
-      </div>
-    </>
+    </div>
   );
 }
