9cda33d76953a6b98e5325375740368d8cf015e5
Jay Varner <jayvarner@gmail.com>
2023-03-14 08:09:36 -0400

    Pass sizes to button component

--- app/components/FancyButton.tsx
+++ app/components/FancyButton.tsx
@@ -1,13 +1,15 @@
-export default function FancyButton({ action, outlineColor, children }) {
+export default function FancyButton({ action, outlineColor, children, width, height, fontSize, x, y }) {
   return (
     <svg
       onClick={action}
       type="button"
-      width={77}
-      height={32}
+      width={width ?? 77}
+      height={height ?? 32}
       viewBox="0 0 77 32"
       fill="none"
       role="button"
+      x={x ?? 0}
+      y={y ?? 0}
     >
       <path
         d="M70 31H7C4.66 28.66 3.34 27.34 1 25V7C3.34 4.66 4.66 3.34 7 1H70C72.34 3.34 73.66 4.66 76 7V25C73.66 27.34 72.34 28.66 70 31Z"
@@ -21,6 +23,7 @@ export default function FancyButton({ action, outlineColor, children }) {
         dominantBaseline="middle"
         className="font-dubois"
         fill={outlineColor ?? "white"}
+        fontSize={fontSize}
       >
         {children}
       </text>
