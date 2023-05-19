7dee305cf48120d38b0660ee8821cc80d127cfc0
Jay Varner <jayvarner@gmail.com>
2023-05-19 09:29:54 -0400

    Add label to toggle switch

--- app/components/Toggle.tsx
+++ app/components/Toggle.tsx
@@ -1,8 +1,10 @@
 import type { Dispatch, SetStateAction } from "react";
 import { Switch } from "@headlessui/react";
+import type { ReactNodeLike } from "prop-types";
 
 interface Props {
   checked: boolean;
+  children: ReactNodeLike;
   colorOff: string;
   colorOn: string;
   onChange: Dispatch<SetStateAction<boolean>>;
@@ -11,26 +13,32 @@ interface Props {
 
 export default function Toggle({
   checked,
+  children,
   onChange,
   colorOn,
   colorOff,
   screenReaderMsg,
 }: Props) {
   return (
-    <Switch
-      checked={checked}
-      onChange={onChange}
-      className={`bg-${
-        checked ? colorOn : colorOff
-      } relative inline-flex h-6 w-11 items-center rounded-full outline outline-2 outline-white`}
-    >
-      <span className="sr-only">{screenReaderMsg}</span>
-      <span
-        className={`${
-          checked ? `translate-x-6 bg-white` : `translate-x-1 bg-${colorOn}`
-        } inline-block h-4 w-4 transform rounded-full transition`}
-      />
-    </Switch>
+    <Switch.Group>
+      <Switch
+        checked={checked}
+        onChange={onChange}
+        className={`bg-${
+          checked ? colorOn : colorOff
+        } relative inline-flex h-6 w-11 items-center rounded-full outline outline-2 outline-white`}
+      >
+        <span className="sr-only">{screenReaderMsg}</span>
+        <span
+          className={`${
+            checked ? `translate-x-6 bg-white` : `translate-x-1 bg-${colorOn}`
+          } inline-block h-4 w-4 transform rounded-full transition`}
+        />
+      </Switch>
+      <Switch.Label className="ml-4">
+        {children}
+      </Switch.Label>
+    </Switch.Group>
   );
 }
 
