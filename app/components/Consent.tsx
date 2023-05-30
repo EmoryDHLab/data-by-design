60841fc28b5b4429c9c4619f057df95047d4f754
Jay Varner <jayvarner@gmail.com>
2023-05-30 14:25:26 -0400

    Changes to Brooks scrollytell

--- app/components/Consent.tsx
+++ app/components/Consent.tsx
@@ -37,19 +37,20 @@ export default function Consent() {
   }, [setSmall, docHeightState]);
 
   return (
-    <div ref={containerRef} className={`w-full border-b-2 border-slate-400 sticky -top-4 grid-wrapper mx-12 md:mx-24 md:py-5 text-center z-10 bg-${accentColor}`}>
-      <p>
+    <div ref={containerRef} className={`w-full border-b-2 border-slate-400 sticky -top-4 mx-12 md:mx-24 md:pt-5 z-10 bg-${accentColor}`}>
+      <p className="text-center w-full">
         <strong>Note:</strong> This chapter deals with images of slavery.
       </p>
-      <p className={`w-full transition-transform duration-700 ${small ? "-translate-x-3/4 scale-75 h-1" : ""}`}>
+      {/* <p className={`transition-all duration-700 ${small ? "md:-translate-x-3/4 scale-75 h-1" : ""}`}> */}
+      <p className={`text-center transition-all duration-1000 mx-0 pt-3 md:pt-0 ${small ? "md:w-1 md:mx-16 md:scale-75" : "md:w-full"}`} style={{ whiteSpace: "nowrap"}}>
         <Toggle
-          checked={hideSensitiveState}
-          onChange={setHideSensitiveState}
+          checked={!hideSensitiveState}
+          onChange={() => setHideSensitiveState(!hideSensitiveState)}
           colorOn={backgroundColor}
           colorOff={accentColor}
           screenReaderMsg="Consent to descriptions of images of slavery."
         >
-          Hide sensitive images
+          Show sensitive images
         </Toggle>
       </p>
     </div>
