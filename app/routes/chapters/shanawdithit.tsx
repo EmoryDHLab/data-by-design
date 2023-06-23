bbf0f3fb6d11966ec2c31967dfb08cefc8a0c0a4
Jay Varner <jayvarner@gmail.com>
2023-06-23 11:47:05 -0400

    Clean up empty Shanawdithit

--- app/routes/chapters/shanawdithit.tsx
+++ app/routes/chapters/shanawdithit.tsx
@@ -1,13 +1,29 @@
+import { ChapterContext } from "~/chapterContext";
 import ChapterTitle from "~/components/ChapterTitle";
+import Footer from "~/components/Footer";
+import CenteredLayout from "~/components/layout/CenteredLayout";
 
 
 export default function ShanawdithitPage() {
   return (
-    <div>
-      <ChapterTitle
-        title="Narratives of Possession"
-        subtitle="Shanawdithit’s Narrative Maps"
-      />
-    </div>
+      <ChapterContext.Provider
+        value={{
+          backgroundColor: "shanawdithitPrimary",
+          primaryTextColor: "white",
+          accentColor: "shanawdithitPrimary",
+          footnoteColor: "shanawdithitPrimary",
+        }}
+      >
+        <ChapterTitle
+          title="Narratives of Possession"
+          subtitle="Shanawdithit's Narrative Maps"
+        />
+        <CenteredLayout>
+          <p>
+            Content can go here.
+          </p>
+        </CenteredLayout>
+        <Footer />
+      </ChapterContext.Provider>
   );
 }
