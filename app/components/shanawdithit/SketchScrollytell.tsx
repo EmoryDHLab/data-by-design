253f88a7765fc18993968936a2a0fd4049a67603
Jay Varner <jayvarner@gmail.com>
2023-10-26 08:38:45 -0400

    Updates to maps scrollytell

--- app/components/shanawdithit/SketchScrollytell.tsx
+++ app/components/shanawdithit/SketchScrollytell.tsx
@@ -5,8 +5,8 @@ import ScrollytellWrapper from "~/components/ScrollytellWrapper";
 import Triggers from "./sketchScrollytell/Triggers";
 import type { TFigure } from "~/types/figureType";
 
-const width = 793;
-const height = 573;
+const width = 3482;
+const height = 2467;
 
 interface Props {
   figure: TFigure;
@@ -16,81 +16,76 @@ function SketchScrollytell({ figure }: Props) {
   const { accentTextColor } = useContext(ChapterContext);
   const [scrollProgress, setScrollProgress] = useState<number>(0.0);
   const [focusShapeSize, setFocusShapeSize] = useState<object>({ x: 0, y: 0, width, height });
-  const [zoom, setZoom] = useState<number>(1);
-  const [xOffset, setXOffset] = useState<number>(0);
-  const [yOffset, setYOffset] = useState<number>(0);
+  const [zoom, setZoom] = useState<number>(2);
+  const [translateX, setTranslateX] = useState<number>(0);
+  const [translateY, setTranslateY] = useState<number>(60);
   const steps = useRef<HTMLDivElement>(null);
 
   const minScrollProgress = 0;
 
   useEffect(() => {
     switch(true) {
-      // Cross-sections
-      // case (scrollProgress >= minScrollProgress && scrollProgress < minScrollProgress + 1):
-      //   setFocusShapeSize({ x: 0, y: 650, width, height: 800});
-      //   setZoom(1);
-      //   setXOffset(0);
-      //   setYOffset(0);
-      //   break;
-      // Side Cross-section
+      // Winter Camp
       case (scrollProgress >= minScrollProgress + 1 && scrollProgress < minScrollProgress + 2):
-        setFocusShapeSize({ x: 500, y: 375, width: 95, height: 75});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 1400, y: 1500, width: 800, height: 800 });
+        setZoom(3);
+        setTranslateX(-600);
+        setTranslateY(-250);
         break;
-      // Text
+      // Dotted Paths
       case (scrollProgress >= minScrollProgress + 2 && scrollProgress < minScrollProgress + 3):
-        setFocusShapeSize({ x: 200, y: 250, width: 250, height: 125});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 230, y: 675, width: 3000, height: 1200 });
+        setZoom(2);
+        setTranslateX(550);
+        setTranslateY(-25);
         break;
-      // Title
+      // North Shore
       case (scrollProgress >= minScrollProgress + 3 && scrollProgress < minScrollProgress + 4):
-        setFocusShapeSize({ x: 415, y: 195, width: 50, height: 50});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 1600, y: 900, width: 400, height: 400 });
+        setZoom(4.5);
+        setTranslateX(-120);
+        setTranslateY(300);
         break;
-      // Tables
+      // 2nd set of mamateeks
       case (scrollProgress >= minScrollProgress + 4 && scrollProgress < minScrollProgress + 5):
-        setFocusShapeSize({ x: 165, y: 165, width: 50, height: 50});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 1550, y: 775, width: 400, height: 400});
+        setZoom(4.5);
+        setTranslateX(900);
+        setTranslateY(400);
         break;
-        // Table 1
+        // Setter surveillance
       case (scrollProgress >= minScrollProgress + 5 && scrollProgress < minScrollProgress + 6):
-        setFocusShapeSize({ x: 560, y: 215, width: 50, height: 50});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 1450, y: 1100, width: 400, height: 400 });
+        setZoom(4.5);
+        setTranslateX(-800);
+        setTranslateY(300);
         break;
-      // Table 2
+      // Safety in the woods
       case (scrollProgress >= minScrollProgress + 6 && scrollProgress < minScrollProgress + 7):
-        setFocusShapeSize({ x: 325, y: 100, width: 150, height: 150});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 925, y: 200, width: 1600, height: 2100});
+        setZoom(3.5);
+        setTranslateX(0);
+        setTranslateY(525);
         break;
+      // Capture
       case (scrollProgress >= minScrollProgress + 7 && scrollProgress < minScrollProgress + 8):
-        setFocusShapeSize({ x: 425, y: 225, width: 95, height: 75});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 925, y: 500, width: 1200, height: 1200});
+        setZoom(3.5);
+        setTranslateX(-400);
+        setTranslateY(150);
         break;
+      // Captured
       case (scrollProgress >= minScrollProgress + 8 && scrollProgress < minScrollProgress + 9):
-        setFocusShapeSize({ x: 550, y: 230, width: 40, height: 40});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setFocusShapeSize({ x: 1450, y: 1100, width: 400, height: 400 });
+        setZoom(4.5);
+        setTranslateX(-715);
+        setTranslateY(245);
         break;
       default:
         setFocusShapeSize({ x: 0, y: 0, width, height});
-        setZoom(1);
-        setXOffset(0);
-        setYOffset(0);
+        setZoom(2);
+        setTranslateX(0);
+        setTranslateY(60);
       }
   }, [scrollProgress]);
 
@@ -108,15 +103,18 @@ function SketchScrollytell({ figure }: Props) {
           <div className="sticky p-8 md:p-0 top-16 h-min mt-16 bias-full w-full md:bias-1/2 md:w-7/12 md:order-last">
             <svg
               viewBox={`0 0 ${width} ${height}`}
-              className="max-h-[80vh] max-w-[90%] md:my-16 mx-auto drop-shadow-2xl"
+              className="max-h-[60vh] max-w-[90%] md:mt-[33%] md:mb-16 mx-auto drop-shadow-2xl"
             >
               <image
                 href={`/images/${figure.chapter}/${figure.fileName}.jpg`}
                 className="transition-all origin-center duration-1000  opacity-100"
-                width={width * zoom}
-                height={height * zoom}
-                x={0 + xOffset}
-                y={0 + yOffset}
+                width={width}
+                height={height}
+                x={0}
+                y={0}
+                style={{
+                  transform: `scale(${zoom}) translateY(${translateY}px) translateX(${translateX}px)`
+                }}
               />
               <defs>
                 <mask id="sketch-mask">
@@ -126,7 +124,7 @@ function SketchScrollytell({ figure }: Props) {
                     width={width}
                     height={height}
                     fill="white"
-                    fillOpacity={0.5}
+                    fillOpacity={0.7}
                     className="transition-all duration-1000"
                   />
                   <rect
