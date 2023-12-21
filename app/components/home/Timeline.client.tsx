e5f2b1f8cfcffa0fcafd51b6fc95e96e474c54a3
Jay Varner <jayvarner@gmail.com>
2023-12-21 12:38:29 -0500

    Change timeline heading element

--- app/components/home/Timeline.client.tsx
+++ app/components/home/Timeline.client.tsx
@@ -20,7 +20,7 @@ export default function Timeline({
   return (
     <div className="text-white text-center font-duboisWide text-3xl p-10 bg-black hidden md:block">
       <div className="flex items-center border-white border-dashed border-y-2 w-full">
-        <h1 className="-rotate-90 h-[20px]">TIMELINE</h1>
+        <h2 className="-rotate-90 h-[20px]">TIMELINE</h2>
         {timelineType === TimelineType.Draggable ? (
           <DraggableTimeline
             selectedImage={selectedImage}
