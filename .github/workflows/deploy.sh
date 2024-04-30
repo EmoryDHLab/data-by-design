d81df9bddd96448557f044057bb4c12c328091d2
Jay Varner <jayvarner@gmail.com>
2024-04-30 11:09:27 -0400

    Fix repo url

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,12 +8,12 @@ aws ecr get-login-password --region us-east-1 | \
 docker login --username AWS --password-stdin "${AWS_ECR}"
 echo "Logged in successfully"
 
-echo "Would tag image as ${AWS_ECR}:${TAG}"
+echo "Would tag image as ${AWS_ECR}/data-by-design:${TAG}"
 echo "Bransh is ${BRANCH}"
 
 echo "Building and tagging Docker image"
 docker build -t dxd --no-cache .
-docker tag dxd "${AWS_ECR}:${TAG}"
+docker tag dxd "${AWS_ECR}/data-by-design:${TAG}"
 
 echo "Pushing image"
 docker push "${AWS_ECR}:${TAG}"
