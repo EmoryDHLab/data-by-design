4260415777bf049c421dbc0376e1fcd79c9d9d76
Nicholas Yang <nick@nicholasyang.com>
2024-05-02 21:57:18 -0400

    Impl

---
+++ app/components/intro/MinardBlackLine.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+const MinardBlackLine = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={728}
+    height={193}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#1C1817"
+      strokeWidth={4}
+      d="m437 133.5-79.5 16m.5-.101L298.5 175.5M261 62.5l35 113m-44.5 10-52-29.5-30 16.5-27.5-8-66-3.5-28.75.75-28.75.75-17.5 1"
+    />
+    <path stroke="#1C1817" d="M27 32.5 18.5 163" />
+    <path stroke="#1C1817" strokeWidth={10} d="m300 176-45.5 11.5-7.5-1" />
+    <path stroke="#1C1817" strokeWidth={6} d="m508.5 106.5-73 27" />
+    <path stroke="#1C1817" strokeWidth={10} d="m564 76-55 34" />
+    <path
+      stroke="#1C1817"
+      strokeWidth={15}
+      d="m724 7-11 6-4 51-13 6.5L618.5 53 563 79"
+    />
+  </svg>
+);
+export default MinardBlackLine;
