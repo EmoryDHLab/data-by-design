f8e951e4005552c6ccc5573c850e9761a5c04e87
Jay Varner <jayvarner@gmail.com>
2024-05-08 15:06:44 -0400

    Forgot to include update to CardGrid

--- app/components/home/ChapterCardGrid.tsx
+++ app/components/home/ChapterCardGrid.tsx
@@ -1,19 +1,19 @@
 import ChapterCard from "~/components/home/ChapterCard";
-import chapters from "~/data/chapterMeta.json";
+import { chapterMeta } from "~/data/chapterMeta";
 import type { TChapterMeta, ChapterTitle } from "~/types/chapterMetaTags";
 
 const ChapterCardGrid = () => {
   return (
     <div className="bg-black ">
       <div className="grid md:grid-cols-2 grid-cols-1 md:grid-rows-3 3xl:mx-48">
-        {Object.keys(chapters as TChapterMeta)
+        {Object.keys(chapterMeta as TChapterMeta)
           .slice(1, 8)
           .map((chapterName) => {
-            const chapter = chapters[chapterName as ChapterTitle];
+            const chapter = chapterMeta[chapterName as ChapterTitle];
             return (
               <ChapterCard
                 key={`card-${chapterName}`}
-                to={`/chapters/${chapterName}`}
+                to={`/chapterMeta/${chapterName}`}
                 className={`hover:bg-${chapterName} bg-center hover:text-white`}
                 title={chapter.title}
                 subtitle={chapter.subtitle}
