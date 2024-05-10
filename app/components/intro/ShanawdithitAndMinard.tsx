fb723a9bc4c2e1aabd2c0a8c74ab8a5c236ae972
Jay Varner <jayvarner@gmail.com>
2024-05-10 10:23:07 -0400

    Make images side by side

--- app/components/intro/ShanawdithitAndMinard.tsx
+++ app/components/intro/ShanawdithitAndMinard.tsx
@@ -12,17 +12,32 @@ export default function ShanawdithitAndMinard() {
     rightOpacity = Math.max(100 - (scrollProgress - 20) * 100, 20);
   }
 
+  // return (
+  //   <div className="relative w-[80vw]">
+  //     <img
+  //       style={{ left: `${offset}vw`, opacity: leftOpacity }}
+  //       className="absolute max-w-lg transition"
+  //       src="/images/intro/1-Carte_figurative_des_pertes_successives_Minard_Charles-Joseph.jpeg"
+  //       alt=""
+  //     />
+  //     <img
+  //       style={{ right: `${offset}vw`, opacity: rightOpacity }}
+  //       className="absolute max-w-lg top-12 transition"
+  //       src="/images/shanawdithit/1829-03.webp"
+  //       alt=""
+  //     />
+  //   </div>
+  // );
+
   return (
-    <div className="relative w-[80vw]">
+    <div className="relative flex space-x-12 w-screen items-center justify-center">
       <img
-        style={{ left: `${offset}vw`, opacity: leftOpacity }}
-        className="absolute max-w-lg transition"
+        className="max-w-lg transition"
         src="/images/intro/1-Carte_figurative_des_pertes_successives_Minard_Charles-Joseph.jpeg"
         alt=""
       />
       <img
-        style={{ right: `${offset}vw`, opacity: rightOpacity }}
-        className="absolute max-w-lg top-12 transition"
+        className="max-w-lg top-12 transition"
         src="/images/shanawdithit/1829-03.webp"
         alt=""
       />
