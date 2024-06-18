5eb9928a1df5ea209d0b2bf067ce1c2fa88533ed
Jay Varner <jayvarner@gmail.com>
2024-06-18 10:51:33 -0400

    Increase size of and spacing for consent toggle.

--- app/components/consent/Consent.tsx
+++ app/components/consent/Consent.tsx
@@ -12,10 +12,13 @@ export default function Consent() {
   if (setHideSensitiveState && windowSize.height) {
     return (
       <div
-        className={`absolute z-50 right-0 mr-2 text-offblack pointer-events-auto`}
-        style={{ top: `${windowSize.height - 92}px` }}
+        className={`absolute z-50 right-8 mr-2 text-offblack pointer-events-auto`}
+        // 48 = offset for navbar
+        // 56 = height of icon (h-14, 3.5rem, 56px)
+        // 32 = offset to match right-8 (2rem, 32px)
+        style={{ top: `${windowSize.height - (48 + 56 + 32)}px` }}
       >
-        <ConsentToggle />
+        <ConsentToggle className="h-14 w-14" />
         <Tooltip id="consent-toggle" />
       </div>
     );
