676e3f0cd434c7801117bb8f2d0e27c26865c023
Jay Varner <jayvarner@gmail.com>
2024-08-01 12:51:18 -0400

    Improve loading state

--- app/components/layout/Loading.tsx
+++ app/components/layout/Loading.tsx
@@ -4,7 +4,7 @@ const Loading = () => {
   const navigation = useNavigation();
   return (
     <div
-      className={`absolute flex items-center text-center h-screen w-screen top-0 z-50 bg-black/50 pointer-events-none transition-opacity duration-700 opacity-${
+      className={`fixed flex items-center text-center h-screen w-screen top-0 z-50 bg-black/50 pointer-events-none transition-opacity duration-700 opacity-${
         navigation.state == "loading" ? 100 : 0
       }`}
     >
