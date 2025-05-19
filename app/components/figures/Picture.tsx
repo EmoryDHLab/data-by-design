9fe357be14c84d2b687e2891edbbd4081437da85
Jay Varner <jayvarner@gmail.com>
2025-05-19 19:09:46 -0400

    Use webp not png

--- app/components/figures/Picture.tsx
+++ app/components/figures/Picture.tsx
@@ -12,17 +12,21 @@ interface Props {
 const Picture = ({ figure, className, center = true }: Props) => {
   const { hideSensitiveState } = useContext(ChapterContext);
   const pictureRef = useRef<HTMLPictureElement>(null);
-  const [figurePath, setFigurePath] = useState<string>(`/images/${figure.chapter}/${figure.fileName}`);
+  const [figurePath, setFigurePath] = useState<string>(
+    `/images/${figure.chapter}/${figure.fileName}`
+  );
 
   useEffect(() => {
     if (figure.width) {
-      setFigurePath(`https://iiif.ecds.io/iiif/3/dxd/${figure.chapter}/${figure.fileName}.tiff/full/max/0/color`)
+      setFigurePath(
+        `https://iiif.ecds.io/iiif/3/dxd/${figure.chapter}/${figure.fileName}.tiff/full/max/0/color`
+      );
     }
   }, [figure]);
 
   return (
     <picture ref={pictureRef}>
-      <source srcSet={`${figurePath}.png`} />
+      <source srcSet={`${figurePath}.webp`} />
       <source srcSet={`${figurePath}.jpg`} />
       <img
         className={classNames(center ? "mx-auto" : "mx-0", className)}
