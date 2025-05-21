2fca5eb27adff2339baeb2eeb2f4addc34c4a11b
Jay Varner <jayvarner@gmail.com>
2025-05-21 10:25:02 -0400

    Set NODE_ENV for build

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,14 +5,20 @@ echo "Running deploy script"
 REMOTE_PATH=$([ "$BRANCH" == "main" ] && echo $PROD_REMOTE_PATH || echo $DEV_REMOTE_PATH)
 RESTART_COMMAND=$([ "$BRANCH" == "main" ] && echo $PROD_RESTART_COMMAND || echo $DEV_RESTART_COMMAND)
 
-echo "Building"
+if [ $BRANCH = "main" ]; then
+  export NODE_ENV="production"
+else
+  export NODE_ENV="develop"
+fi
+
+echo "Building for ${NODE_ENV}"
 npm run build
 
 echo "Copying Files"
 files=("./build" "./package.json" "./server.*" "./server" "./node_modules")
 for file in "${files[@]}"; do
   echo "Copying ${file} to ${REMOTE_PATH}"
-  rsync -avze "ssh" ${file} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+  rsync -ave "ssh" ${file} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 done
 
 echo "Running Remote Script"
