b4c16066f9020c6e60f3ebf148db9ea84517d956
Jay Varner <jayvarner@gmail.com>
2025-05-21 09:48:29 -0400

    Another deploy fix

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Running deploy script"
 
-REMOTE_PATH=$([ "$BRANCH" == "main" ] && echo $PROD_RESTART_COMMAND || echo $DEV_RESTART_COMMAND)
+REMOTE_PATH=$([ "$BRANCH" == "main" ] && echo $PROD_REMOTE_PATH || echo $DEV_REMOTE_PATH)
 RESTART_COMMAND=$([ "$BRANCH" == "main" ] && echo $PROD_RESTART_COMMAND || echo $DEV_RESTART_COMMAND)
 
 echo "Building"
@@ -11,6 +11,7 @@ npm run build
 echo "Copying Files"
 files=("./build" "./package.json" "./server.*" "./server" "./node_modules")
 for file in "${files[@]}"; do
+  echo "Copying ${file} to ${REMOTE_PATH}"
   rsync -avze "ssh" ${file} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 done
 
