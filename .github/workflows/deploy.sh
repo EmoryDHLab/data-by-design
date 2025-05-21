266a9cb6e561f8b2746b32e32668e5472fb155b4
Jay Varner <jayvarner@gmail.com>
2025-05-21 09:35:18 -0400

    New deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
+set -e
 echo "Running deploy script"
-TAG=$([ "$BRANCH" == "main" ] && echo "latest" || echo "dev")
 
-echo "Logging in to AWS"
-aws ecr get-login-password --region us-east-1 | \
-docker login --username AWS --password-stdin "${AWS_ECR}"
-echo "Logged in successfully"
+REMOTE_PATH=$([ "$BRANCH" == "main" ] && echo $PROD_RESTART_COMMAND || echo $DEV_RESTART_COMMAND)
+RESTART_COMMAND=$([ "$BRANCH" == "main" ] && echo $PROD_RESTART_COMMAND || echo $DEV_RESTART_COMMAND)
 
-echo "Building Docker image for ${BRANCH}"
-if [ "$BRANCH" == "main" ]; then
-  docker build -t data-by-design --no-cache .
-else
-  docker build -t data-by-design --no-cache --file Dockerfile-dev .
-fi
+echo "Building"
+npm run build
 
-echo "Tagging image with ${TAG}"
-docker tag data-by-design "${AWS_ECR}/data-by-design:${TAG}"
+echo "Copying Files"
+files=("./build" "./package.json" "./server.*" "./server" "./node_modules")
+for file in "${files[@]}"; do
+  rsync -avze "ssh" ${file} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+done
 
-echo "Pushing image"
-docker push "${AWS_ECR}/data-by-design:${TAG}"
-
-echo "Forcing new deployment"
-if [ "$BRANCH" == "main" ]; then
-  aws ecs update-service --cluster ${AWS_ECS_CLUSTER} --service ${AWS_ECS_SERVICE} --force-new-deployment --region ${AWS_REGION}
-else
-  aws ecs update-service --cluster ${AWS_ECS_CLUSTER_DEV} --service ${AWS_ECS_SERVICE_DEV} --force-new-deployment --region ${AWS_REGION}
-fi
+echo "Running Remote Script"
+ssh ${REMOTE_USER}@${REMOTE_HOST} ${RESTART_COMMAND}
