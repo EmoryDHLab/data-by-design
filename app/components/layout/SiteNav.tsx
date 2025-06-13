df36b34333d2ba324dff7de8554a94a6cb914594
Jay Varner <jayvarner@gmail.com>
2025-06-13 10:38:01 -0400

    Improve site nav for mobile.

--- app/components/layout/SiteNav.tsx
+++ app/components/layout/SiteNav.tsx
@@ -22,11 +22,11 @@ const SiteNav = () => {
   return (
     <>
       <nav
-        className={`flex flex-col md:flex-row bg-black/95 text-offwhite font-power fixed top-0 bottom-0 w-screen h-screen transition-transform duration-500 z-50 overflow-y-hidden overflow-x-hidden md:pt-24 ${
+        className={`flex flex-col md:flex-row bg-black/95 text-offwhite font-power fixed top-0 bottom-0 w-screen h-auto md:h-screen transition-transform duration-500 z-50 overflow-y-scroll md:overflow-y-hidden overflow-x-hidden md:pt-24 ${
           show ? "translate-y-0" : "-translate-y-full"
         }`}
       >
-        <ol className="w-full text-2xl ps-8">
+        <ol className="w-full text-2xl ps-8 mt-12 md:mt-0">
           <li className="mb-4">
             <Link to="/">DxD</Link>
           </li>
@@ -45,9 +45,11 @@ const SiteNav = () => {
                   {index < 2 ? (
                     ""
                   ) : (
-                    <div className="text-xs pt-2 ps-2">Chapter {index - 1}</div>
+                    <div className="text-xs pt-2 ps-8 md:ps-2">
+                      Chapter {index - 1}
+                    </div>
                   )}
-                  <div className="capitalize flex flex-row md:text-xl pt-2 ps-2">
+                  <div className="capitalize flex flex-row md:text-xl pt-2 ps-8 md:ps-2">
                     <div className="flex-grow">{chapter}</div>
                     <div className="font-icons normal-case me-8">b</div>
                   </div>
