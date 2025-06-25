93f4906f65bed0d8fe60650bbab5e417f659292e
Jay Varner <jayvarner@gmail.com>
2025-06-25 14:25:38 -0400

    Add padding to consent toggle

--- app/components/consent/Consent.tsx
+++ app/components/consent/Consent.tsx
@@ -14,7 +14,7 @@ export default function Consent() {
       <div
         className={`absolute z-50 md:hidden lg:block right-5 md:right-6 lg:right-14 top-[calc(100vh-6rem)] md:top-[calc(100vh-8rem)] lg:top-[calc(100vh-10rem)] text-offblack pointer-events-auto`}
       >
-        <ConsentToggle className="h-12 w-12 md:h-16 md:w-16 lg:h-18 lg:w-18" />
+        <ConsentToggle className="h-12 w-12 md:h-16 md:w-16 lg:h-18 lg:w-18 p-1.5" />
         <Tooltip id="consent-toggle" />
       </div>
     );
