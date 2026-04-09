c535f98df57fd759dd787ba70c7bb85eaac7f606
Jay Varner <jayvarner@gmail.com>
2026-04-09 10:21:27 -0400

    Fix urls for ordered timeline

--- app/components/home/OrderedTimeline.tsx
+++ app/components/home/OrderedTimeline.tsx
@@ -154,11 +154,11 @@ const OrderedTimeline = ({ setSelectedImage, selectedImage }: Props) => {
                 >
                   <picture>
                     <source
-                      srcSet={`https://iiif.ecds.io/iiif/3/dxd%2fimage.chapter}/${image.fileName}.tiff/full/300,/0/default.png`}
+                      srcSet={`https://iiif.ecds.io/iiif/3/dxd%2f${image.chapter}%2f${image.fileName}.tiff/full/300,/0/default.png`}
                       type="image/webp"
                     />
                     <source
-                      srcSet={`https://iiif.ecds.io/iiif/3/dxd%2fimage.chapter}/${image.fileName}.tiff/full/300,/0/default.jpg`}
+                      srcSet={`https://iiif.ecds.io/iiif/3/dxd%2f${image.chapter}%2f${image.fileName}.tiff/full/300,/0/default.jpg`}
                       type="image/jpeg"
                     />
                     <img
