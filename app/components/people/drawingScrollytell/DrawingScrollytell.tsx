0c2d5f4d54e561765fd5973737b99b178e89f41f
Jay Varner <jayvarner@gmail.com>
2026-09-15 12:10:11 -0400

    Improve scrollytell animation

--- app/components/people/drawingScrollytell/DrawingScrollytell.tsx
+++ app/components/people/drawingScrollytell/DrawingScrollytell.tsx
@@ -25,16 +25,18 @@ function SketchScrollytell({ figure, triggers }: Props) {
       bgColor="peopleSecondary"
     >
       <div
-        className={`flex flex-col md:flex-row justify-between`}
+        className={`flex flex-col md:flex-row justify-center`}
         id="scrollytell-one"
       >
-        <div className="sticky p-8 md:p-0 top-0 h-min mt-16 mr-6 bias-full w-full md:bias-1/2 md:w-7/12 md:order-last rounded-md ">
-          <ClientOnly>
-            <IIIFViewer
-              figure={figure.fileName}
-              scrollProgress={scrollProgress}
-            />
-          </ClientOnly>
+        <div className="sticky top-4 md:top-8 h-[calc(100vh-1rem)] md:h-[calc(100vh-2rem)] mt-16 mr-6 flex items-center justify-center bias-full w-full md:bias-1/2 md:w-7/12 md:order-last">
+          <div className="p-8 md:p-0 w-full rounded-md">
+            <ClientOnly>
+              <IIIFViewer
+                figure={figure.fileName}
+                scrollProgress={scrollProgress}
+              />
+            </ClientOnly>
+          </div>
         </div>
         <div
           ref={steps}
@@ -45,9 +47,9 @@ function SketchScrollytell({ figure, triggers }: Props) {
               <div
                 key={`sketch-trigger-${trigger.key}`}
                 data-step={index}
-                className={`pointer-events-none step text-xl content-center relative min-h-screen text-${accentTextColor}`}
+                className={`pointer-events-none step text-xl content-center relative min-h-[150vh] text-${accentTextColor}`}
               >
-                <span className="bg-peoplePrimary/75">{trigger}</span>
+                <span className="bg-peopleSecondary/75 block">{trigger}</span>
               </div>
             );
           })}
